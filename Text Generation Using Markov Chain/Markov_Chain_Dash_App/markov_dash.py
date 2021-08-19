883f954a1cf813bd2228a8fb3480c819167c603b
Sandip Sonawane <sandips.nemesis@gmail.com>
2021-08-19 09:30:06 -0500

    loaded markov model into dash app

--- Text Generation Using Markov Chain/Markov_Chain_Dash_App/markov_dash.py
+++ Text Generation Using Markov Chain/Markov_Chain_Dash_App/markov_dash.py
@@ -68,67 +68,6 @@ app.layout = html.Div([
 ])
 
 
-# Combine all text files
-def combine_txt_files(text_files, path=os.path.join(os.getcwd(), f'Data\\sherlock\\')):
-    corpus = []
-    for file in text_files:
-        with open(os.path.join(path, file)) as f:
-            for line in f:
-                line = line.strip()
-                if line != '':
-                    corpus.append(line)
-    return corpus
-
-
-txt_files = os.listdir(os.path.join(os.getcwd(), 'Data\\sherlock\\'))
-all_text = combine_txt_files(txt_files)
-
-
-# Tokenizing text
-def tokenize_text(text):
-    tokenized_text = []
-    for line in text:
-        line = line.lower()
-        tokens = word_tokenize(line)
-        tokenized_text.extend([w for w in tokens if w.isalpha()])
-    return tokenized_text
-
-
-tokenized_text = tokenize_text(all_text)
-
-
-# Markov Model
-
-def build_morkov_model(text, n_gram=2):
-    markov_model = {}
-
-    # create dictionary having no. of transitions from current state to next state
-    for i in range(len(text) - n_gram - 1):
-        state_zero, state_one = "", ""
-        for j in range(n_gram):
-            state_zero += text[i + j] + " "
-            state_one += text[i + j + n_gram] + " "
-        if not state_zero in markov_model:
-            markov_model[state_zero] = {}
-            markov_model[state_zero][state_one] = 1
-        else:
-            if state_one in markov_model[state_zero]:
-                markov_model[state_zero][state_one] += 1
-            else:
-                markov_model[state_zero][state_one] = 1
-
-    # calculate transition probabilities based on transition frequencies
-    for state_zero, transition in markov_model.items():
-        total_transitions = sum(transition.values())
-        for state, transition_count in transition.items():
-            markov_model[state_zero][state] = transition_count / total_transitions
-
-    return markov_model
-
-
-markov_model = build_morkov_model(tokenized_text)
-
-
 # generating text based on text input
 def generate_text(markov_model_input, limit=50, start='the adventure '):
     state_zero = start
@@ -155,24 +94,21 @@ def update_chapter_text(chapter):
     return chapter_text
 
 
+with open(os.path.join(os.getcwd(), f'Data\\markov_model_JSON.txt')) as json_file:
+    markov_model_json_to_dict = json.load(json_file)
+    markov_model_json_to_dict = json.loads(markov_model_json_to_dict)
+
+        
 @app.callback(Output('generated-text', 'value'),
               [Input('choose_words', 'value')])
 def update_chapter_text(words):
-    # markov_model = {}
-    # with open(os.path.join(os.getcwd(), f'Data\\markov_model_JSON.txt')) as f:
-    #     for line in f:
-    #         (key, val) = line.split()
-    #         markov_model[int(key)] = val
-    #
-    # # with open(os.path.join(os.getcwd(), f'Data\\markov_model_JSON.txt')) as json_file:
-    # #     markov_model = json.load(json_file)
-    words = words.lower().strip()
-    words = words + ' '
+        words = words.lower().strip()
+        words = words + ' '
 
-    generated_text = ''
-    for i in range(10):
-        generated_text += f's_{i}' + ': ' + generate_text(markov_model, start=words, limit=10) + '\n \n'
-    return generated_text
+        generated_text = ''
+        for i in range(10):
+            generated_text += f's_{i}' + ': ' + generate_text(markov_model_json_to_dict, start=words, limit=10) + '\n \n'
+        return generated_text
 
 
 app.run_server()
